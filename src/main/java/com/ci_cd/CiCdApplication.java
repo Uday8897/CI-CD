f1760b8e40bc9d2af118d748547ad350c919db87
Uday Devulapalle <134502561+Uday8897@users.noreply.github.com>
2025-11-29 11:19:25 +0530

    Fix typo in SpringApplication.run method

--- src/main/java/com/ci_cd/CiCdApplication.java
+++ src/main/java/com/ci_cd/CiCdApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CiCdApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.runnn(CiCdApplication.class, args);
+		SpringApplication.run(CiCdApplication.class, args);
 	}
 
 }
